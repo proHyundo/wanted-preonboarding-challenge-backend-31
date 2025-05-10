a867bad64ddb55bd6732689673078cc73f912350
proHyundo <guseh08@knou.ac.kr>
2025-05-10 22:37:26 +0900

    Add initial project setup with Gradle, application.yml, and main class

---
+++ src/main/java/com/wanted/PreOnBoardingCqrsApplication.java
@@ -0,0 +1,11 @@
+package com.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnBoardingCqrsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PreOnBoardingCqrsApplication.class, args);
+    }
+}
